dbe4929ecacece55d6523474d5a52078e848d4eb
Astisme <96465880+Astisme@users.noreply.github.com>
2024-12-18 16:03:32 +0100

    update readme with badges (#3)
    Co-authored-by: Alfredo <a.chissotti@reply.it>

--- action/notSalesforceSetup.js
+++ action/notSalesforceSetup.js
@@ -1,20 +1,58 @@
 // deno-lint-ignore-file no-window
+const authorizedDomainRegex = /^https:\/\/[a-zA-Z0-9.-]+\.lightning\.force\.com\/.*/;
 const page = new URLSearchParams(window.location.search).get("url");
-const textEl = document.querySelector("h3");
-let text;
+const sfsetupTextEl = document.querySelector("h3");
+
+const div = document.createElement("div");
+const prefix = document.createTextNode("This is not a ")
+const strongEl = document.createElement("strong")
+const otherText = document.createTextNode("");
+
+sfsetupTextEl.innerText = "";
+sfsetupTextEl.appendChild(div);
+let insertPrefix = true;
+let strongFirst = true;
+
 if (page != null) { // we're in a salesforce page
-	// switch which button is shown
-	document.getElementById("login").classList.add("hidden");
-	const goSetup = document.getElementById("go-setup");
-	goSetup.classList.remove("hidden");
-	// update the button href to use the domain
-	const goSetupLink = goSetup.querySelector("a");
-	const domain = page.substring(0, page.indexOf("/lightning"));
-	goSetupLink.href = `${domain}/lightning/setup/SetupOneHome/home`;
-	// update the bold on the text
-	text = "This is not a Salesforce Lightning <strong>Setup Page</strong>";
+    let domain;
+
+    try {
+        domain = new URL(page).origin;
+    } catch(error){
+        strongEl.textContent = "Invalid URL";
+        otherText.textContent = " detected."
+        insertPrefix = false;
+    }
+
+    // domain is null if an error occurred
+    if(domain != null){
+        // Validate the domain (make sure it's a Salesforce domain)
+        if (!authorizedDomainRegex.test(page)) {
+            strongEl.textContent = "Invalid Salesforce";
+            otherText.textContent = " domain detected.";
+            insertPrefix = false;
+        } else {
+            // switch which button is shown
+            document.getElementById("login").classList.add("hidden");
+            const goSetup = document.getElementById("go-setup");
+            goSetup.classList.remove("hidden");
+            // update the button href to use the domain
+            goSetup.href = `${domain}/lightning/setup/SetupOneHome/home`;
+            // update the bold on the text
+            otherText.textContent = "Salesforce Lightning";
+            strongEl.textContent = " Setup Page"
+            strongFirst = false;
+        }
+    }
 } else {
-	text = "This is not a <strong>Salesforce Lightning</strong> Setup Page";
+    strongEl.textContent = "Salesforce Lightning";
+    otherText.textContent = " Setup Page";
 }
-textEl.innerText = "";
-textEl.insertAdjacentHTML("beforeend", text);
+insertPrefix && div.appendChild(prefix);
+if(strongFirst){
+    div.appendChild(strongEl);
+    div.appendChild(otherText)
+} else {
+    div.appendChild(otherText)
+    div.appendChild(strongEl);
+}
