966cc5262dedf3edc435a62e98a712573f39a981
Alfredo <a.chissotti@reply.it>
2024-12-18 15:46:38 +0100

    better check on domain

--- action/notSalesforceSetup.js
+++ action/notSalesforceSetup.js
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-window
-const authorizedDomainRegex = /https:\/\/.*\.lightning\.force\.com/;
+const authorizedDomainRegex = /^https:\/\/[a-zA-Z0-9.-]+\.lightning\.force\.com\/.*/;
 const page = new URLSearchParams(window.location.search).get("url");
 const sfsetupTextEl = document.querySelector("h3");
 
@@ -14,23 +14,35 @@ let insertPrefix = true;
 let strongFirst = true;
 
 if (page != null) { // we're in a salesforce page
-    // Validate the domain (make sure it's a Salesforce domain)
-    if (!authorizedDomainRegex.test(page)) {
-        strongEl.textContent = "Invalid Salesforce";
-        otherText.textContent = "domain detected.";
+    let domain;
+
+    try {
+        domain = new URL(page).origin;
+    } catch(error){
+        strongEl.textContent = "Invalid URL";
+        otherText.textContent = " detected."
         insertPrefix = false;
-    } else {
-        // switch which button is shown
-        document.getElementById("login").classList.add("hidden");
-        const goSetup = document.getElementById("go-setup");
-        goSetup.classList.remove("hidden");
-        // update the button href to use the domain
-        const domain = page.substring(0, page.indexOf("/lightning"));
-        goSetup.href = `${domain}/lightning/setup/SetupOneHome/home`;
-        // update the bold on the text
-        otherText.textContent = "Salesforce Lightning";
-        strongEl.textContent = " Setup Page"
-        strongFirst = false;
+    }
+
+    // domain is null if an error occurred
+    if(domain != null){
+        // Validate the domain (make sure it's a Salesforce domain)
+        if (!authorizedDomainRegex.test(page)) {
+            strongEl.textContent = "Invalid Salesforce";
+            otherText.textContent = " domain detected.";
+            insertPrefix = false;
+        } else {
+            // switch which button is shown
+            document.getElementById("login").classList.add("hidden");
+            const goSetup = document.getElementById("go-setup");
+            goSetup.classList.remove("hidden");
+            // update the button href to use the domain
+            goSetup.href = `${domain}/lightning/setup/SetupOneHome/home`;
+            // update the bold on the text
+            otherText.textContent = "Salesforce Lightning";
+            strongEl.textContent = " Setup Page"
+            strongFirst = false;
+        }
     }
 } else {
     strongEl.textContent = "Salesforce Lightning";
