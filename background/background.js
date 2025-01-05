e322a76450000850f7c55bb5300c4a9017d2ec2c
Astisme <tyir1d4e@duck.com>
2025-01-05 15:31:23 +0100

    added jsdocs

--- background/background.js
+++ background/background.js
@@ -2,24 +2,49 @@ const isChrome = navigator.userAgent.includes("Chrome");
 const browserObj = isChrome ? chrome : browser;
 const whyKey = "againWhySalesforce";
 
+/**
+ * Adds the key to the items object and invokes the provided callback.
+ *
+ * @param {Object} items - The items to which the key should be added.
+ * @param {function} callback - The callback to execute after adding the key.
+ */
 function addKey(items, callback) {
 	items.key = whyKey;
 	callback(items);
 }
 
+/**
+ * Retrieves stored data from the browser's storage and invokes the provided callback.
+ *
+ * @param {function} callback - The callback to invoke with the retrieved data.
+ */
 function getStorage(callback) {
 	browserObj.storage.sync.get([whyKey], (items) => {
 		addKey(items, callback);
 	});
 }
 
+/**
+ * Stores the provided tabs data in the browser's storage and invokes the callback.
+ *
+ * @param {Array} tabs - The tabs to store.
+ * @param {function} callback - The callback to execute after storing the data.
+ */
 function setStorage(tabs, callback) {
 	const set = {};
 	set[whyKey] = tabs;
 	browserObj.storage.sync.set(set, () => callback(null));
 }
 
-// need to use message in order to work in private windows
+/**
+ * Listens for incoming messages and processes requests to get, set, or notify about storage changes.
+ * Also handles theme updates and tab-related messages.
+ *
+ * @param {Object} request - The incoming message request.
+ * @param {Object} _ - The sender object (unused).
+ * @param {function} sendResponse - The function to send a response back.
+ * @returns {boolean} Whether the message was handled asynchronously.
+ */
 browserObj.runtime.onMessage.addListener((request, _, sendResponse) => {
 	const message = request.message;
 	if (message == null || message.what == null) {
